1bbd29e193e4752e24b2eb595dfc206dd3f6c969
FaatihahGH <faatihah019@gmail.com>
2026-06-14 05:22:16 +0800

    Initial commit

---
+++ app/src/main/java/com/example/lab8b_faatihah/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.lab8b_faatihah;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.lab8b_faatihah.MapsActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button openMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        openMap = (Button) findViewById(R.id.button);
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+
+    }
+    public void openMap(View view){
+        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+        startActivity(intent);
+    }
+}
